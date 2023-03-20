2b5d559ed89ae8a250747a5e5562bb600b1efc68
Paralock <nathanael.mercaldo@gmail.com>
2023-03-20 13:09:49 -0400

    Added simpler Triangle rendering examples

---
+++ Graphics_Domain/Source/Target_OpenGL_Triangle/src/main/java/Application.java
@@ -0,0 +1,204 @@
+package main.java;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.GL;
+
+import java.nio.FloatBuffer;
+
+import static org.lwjgl.glfw.GLFW.*;
+
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+
+
+
+
+
+
+
+
+
+
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class Application {
+
+    private ShaderProgram shaderProgram;
+
+    private int vaoID;
+    private int vboID;
+
+    // The window handle
+    private long window;
+
+    private void init() {
+
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if ( !glfwInit() )
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        // Configure GLFW
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+
+        // Create the window
+        window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+        if ( window == NULL )
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        // Get the thread stack and push a new frame
+        try ( MemoryStack stack = stackPush() ) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+
+            // Get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(window, pWidth, pHeight);
+
+            // Get the resolution of the primary monitor
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            // Center the window
+            glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        } // the stack frame is popped automatically
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window);
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(window);
+    }
+
+    public void initShaders()
+    {
+        glfwSetWindowTitle(window, "The First Triangle");
+
+        shaderProgram = new ShaderProgram();
+        shaderProgram.attachVertexShader("res/vertex_shader.vs");
+        shaderProgram.attachFragmentShader("res/pixel_shader.fs");
+        shaderProgram.link();
+
+        // Generate and bind a Vertex Array
+        vaoID = glGenVertexArrays();
+        glBindVertexArray(vaoID);
+
+        // The vertices of our Triangle
+        float[] vertices = new float[]
+                {
+                        +0.0f, +0.8f,    // Top coordinate
+                        -0.8f, -0.8f,    // Bottom-left coordinate
+                        +0.8f, -0.8f     // Bottom-right coordinate
+                };
+
+        FloatBuffer verticesBuffer = BufferUtils.createFloatBuffer(vertices.length);
+        verticesBuffer.put(vertices).flip();
+
+        vboID = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferData(GL_ARRAY_BUFFER, verticesBuffer, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0);
+        glBindVertexArray(0);
+    }
+
+
+    public void run() {
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
+        init();
+        initShaders();
+        loop();
+
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    public void render(float delta)
+    {
+        // Clear the screen
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+        // Use our program
+//        shaderProgram.bind();
+//
+//        // Bind the vertex array and enable our location
+//        glBindVertexArray(vaoID);
+//        glEnableVertexAttribArray(0);
+//
+//        // Draw a triangle of 3 vertices
+//        glDrawArrays(GL_TRIANGLES, 0, 3);
+//
+//        // Disable our location
+//        glDisableVertexAttribArray(0);
+//        glBindVertexArray(0);
+//
+//        // Un-bind our program
+//        ShaderProgram.unbind();
+    }
+
+    private void loop() {
+
+        GL.createCapabilities();
+
+        float now, last, delta;
+        last = 0;
+
+        while ( !glfwWindowShouldClose(window) ) {
+
+            now = (float) glfwGetTime();
+            delta = now - last;
+            last = now;
+
+            render(delta);
+
+            glfwSwapBuffers(window); // swap the color buffers
+            glfwPollEvents();
+        }
+    }
+
+    public void dispose()
+    {
+        // Dispose the program
+        shaderProgram.dispose();
+
+        // Dispose the vertex array
+        glBindVertexArray(0);
+        glDeleteVertexArrays(vaoID);
+
+        // Dispose the buffer object
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glDeleteBuffers(vboID);
+    }
+
+    public static void main(String[] args) {
+        new Application().run();
+    }
+
+}
